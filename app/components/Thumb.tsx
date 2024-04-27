70eb1376360c12ee535cf28bec218f4ddfb01c1e
Ian Oderon <ianoderon@gmail.com>
2024-04-28 00:06:14 +0800

    Added download and starred UI

--- app/components/Thumb.tsx
+++ app/components/Thumb.tsx
@@ -1,7 +1,11 @@
+'use client';
 import Link from 'next/link';
 import cx from 'clsx';
 import { StarIcon } from '@heroicons/react/24/outline';
+import { StarIcon as StarredIcon } from '@heroicons/react/16/solid';
+import { ArrowDownTrayIcon } from '@heroicons/react/20/solid';
 import { Checkbox } from 'flowbite-react';
+import { useState } from 'react';
 interface Props {
   image: Image;
   state: {
@@ -11,6 +15,9 @@ interface Props {
 
 function Thumb(props: Props) {
   const { image, state } = props;
+  const [isChecked, setIsChecked] = useState(false);
+  const [isStarred, setIsStarred] = useState(false);
+
   return (
     <div className="relative group/root">
       <Link
@@ -45,14 +52,28 @@ function Thumb(props: Props) {
       </div>
       <div className="group-hover/root:flex hidden rounded bg-neutral-950 bg-opacity-70 absolute w-full h-full left-0 top-0 right-0 bottom-0 flex-col justify-between">
         <div className="px-1.5 py-0.5">
-          <Checkbox />
+          <Checkbox checked={isChecked} onChange={() => setIsChecked(!isChecked)} />
         </div>
-        <Link key={image.path} href={image.path} className={cx('flex-1')} target="_blank" prefetch={false}>
-          <div></div>
-        </Link>
+        <div className="flex items-center justify-center w-full">
+          <Link
+            key={image.path}
+            href={image.path}
+            className={cx(
+              'p-2 border-1 border-neutral-100 bg-white text-neutral-950 bg-opacity-70 rounded-full hover:bg-opacity-100'
+            )}
+            target="_blank"
+            prefetch={false}
+          >
+            <ArrowDownTrayIcon className="w-5" />
+          </Link>
+        </div>
         <div className="px-1 pb-0.5">
-          <button onClick={() => {}}>
-            <StarIcon className="w-5 h-5 text-neutral-200" />
+          <button onClick={() => setIsStarred(!isStarred)}>
+            {isStarred ? (
+              <StarredIcon className="w-5 h-5 text-yellow-400" />
+            ) : (
+              <StarIcon className="w-5 h-5 text-neutral-200" />
+            )}
           </button>
         </div>
       </div>
